a028c0464b71d0cceaa2cdf7811d37801c65ae68
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-10-18 18:11:54 +0200

    Add test print in terminal

--- src/main/java/com/company/CdPlayer.java
+++ src/main/java/com/company/CdPlayer.java
@@ -4,6 +4,10 @@ public class CdPlayer implements MediaPlayer {
 
     private CompactDisc cd;
 
+    public CdPlayer() {
+        this.cd = new BornToRun();
+    }
+
     @Override
     public void play() {
         this.cd.play();
--- src/test/java/com/company/CdPlayerTest.java
+++ src/test/java/com/company/CdPlayerTest.java
@@ -1,14 +1,21 @@
 package com.company;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 
 import static org.junit.Assert.*;
 
 public class CdPlayerTest {
 
+    @Rule
+    public final SystemOutRule log = new SystemOutRule().enableLog();
+
     @Test
     public void testCdPlayer() {
-
+        MediaPlayer cdPlayer = new CdPlayer();
+        cdPlayer.play();
+        assertEquals(log.getLog(), "Playing Born to Run by Bruce Springsteen");
     }
 
 }
